6dedb9170981c8986b00405d9d68367db01d9cd5
Saish Mungase <saishmungase@gmail.com>
2025-06-03 19:32:44 +0530

    client-landing-created

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+
+import { Link } from 'react-router-dom'
+import './App.css'
+
+function App() {
+
+  return <div>
+    <h1>No Gali Gal😯ch and Bakch*di ❌</h1>
+    <Link style={{border : 'solid white 1px', padding : "15px", fontSize : "1.2rem", borderRadius : "15px" }} to={'/meet'}>Go To Meet</Link>
+  </div>
+
+}
+
+export default App
